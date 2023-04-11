8f87b7db43317127a4094501d7896511063da57c
mtzcpd1346 <eduardodonascimento@cvccorp.com.br>
2023-04-11 10:20:58 -0300

    url back

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://backend:8080",
 });
 
 export const getCats = async (id_exhibitor: number, sex: "M" | "F") => {
